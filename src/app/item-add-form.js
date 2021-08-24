31f90b53fe186745fbca3487a8bc6eec51a5feb3
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2021-08-23 22:06:47 -0400

    clear form on submit

--- src/app/item-add-form.js
+++ src/app/item-add-form.js
@@ -1,31 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableFooter from '@material-ui/core/TableFooter';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
 import Button from '@material-ui/core/Button';
-import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
-import IconButton from '@material-ui/core/IconButton';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
-
 import firebase from "firebase/app";
 import "firebase/auth";
-import numeral from 'numeral';
+
+const DEFAULT_FORM_STATE = {
+    nm: '',
+    qty: '',
+    ea: ''
+};
 
 const ItemAddForm = ({ projectKey = 'unspecified project' }) => {
-    const [state, setState] = useState({});
+    const [state, setState] = useState(DEFAULT_FORM_STATE);
 
     function onChange({ target: { name, value } }) {
         setState((prev) => ({
-            ...state,
+            ...prev,
             [name]: value
         }));
     }
@@ -34,7 +24,14 @@ const ItemAddForm = ({ projectKey = 'unspecified project' }) => {
         const { nm, qty, ea } = state;
         if (!nm || !qty || ! ea) return;
 
-        firebase.database().ref(`/open/${projectKey}/${nm}/`).set({ qty, ea });
+        firebase.database()
+            .ref(`/open/${projectKey}/${nm}/`)
+            .set({ qty, ea }, (error) => {
+                if (!error) {
+                  setState(DEFAULT_FORM_STATE);
+                }
+              });
+
     }
     
     return (
